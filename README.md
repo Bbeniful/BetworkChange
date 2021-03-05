6f590eb9893669c9d15793e0b45e1ca8bbaf9b4c
Bbeniful <bbenikeszi@gmail.com>
2021-03-05 17:02:08 +0100

    Add ny type, change tag name
    - Add Service type for init broadcast receiver
    - Set the readme tag 1.0.2

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Step 2. Add the dependency
 
 ```
 	dependencies {
-	        implementation 'com.github.Bbeniful:BetworkChange:1.0.0'
+	        implementation 'com.github.Bbeniful:BetworkChange:1.0.1'
 	}
 ```
 
--- app/BetworkChanges/src/main/java/com/bbeniful/betworklistener/listener/BetworkRegister.kt
+++ app/BetworkChanges/src/main/java/com/bbeniful/betworklistener/listener/BetworkRegister.kt
@@ -1,6 +1,8 @@
 package com.bbeniful.betworklistener.listener
 
 import android.app.Activity
+import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.LocalServerSocket
@@ -21,6 +23,12 @@ object BetworkRegister {
             }
             is Fragment -> {
                 LocalBroadcastManager.getInstance(type.requireContext()).registerReceiver(
+                    BetworkChecker(),
+                    IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+                )
+            }
+            is Service -> {
+                LocalBroadcastManager.getInstance(type.applicationContext).registerReceiver(
                     BetworkChecker(),
                     IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
                 )
